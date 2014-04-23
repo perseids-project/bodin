5facc0b4df738cea1fe0816688b3094551173cab
caesarfeta <adamtavares@gmail.com>
2014-04-23 16:31:27 -0400

    Putting it all together... still...

--- src/js/bodin.js
+++ src/js/bodin.js
@@ -23,19 +23,19 @@
  */
 var Bodin = function() {
 	//------------------------------------------------------------
-	//  This class is a singleton.  Ensure only one instance exists.
+	//	This class is a singleton.	Ensure only one instance exists.
 	//------------------------------------------------------------
 	if ( Bodin.prototype._singleton ) {
 		return Bodin.prototype._singleton;
 	}
 	Bodin.prototype._singleton = this;
 	//------------------------------------------------------------
-	//  Store the config
+	//	Store the config
 	//------------------------------------------------------------
 	this.config = {};
 	this.bodin_align = new BodinAlign();
 	//------------------------------------------------------------
-	//  Events
+	//	Events
 	//------------------------------------------------------------
 	this.events = {
 		loaded: 'Bodin-LOADED'
@@ -46,16 +46,16 @@ var Bodin = function() {
 	this.start = function( _selector ) {
 		var self = this;
 		//------------------------------------------------------------
-		//  Store the configuration
+		//	Store the configuration
 		//------------------------------------------------------------
 		jQuery( _selector ).each( function() {
 			//------------------------------------------------------------
-			//  Keep track of when the content has been loaded
+			//	Keep track of when the content has been loaded
 			//------------------------------------------------------------
 			var id = jQuery( this ).attr('id');
 			var src = jQuery( this ).attr('src');
 			//------------------------------------------------------------
-			//  Setup load listener
+			//	Setup load listener
 			//------------------------------------------------------------
 			jQuery( '#'+id ).on( 'TeiToBodin-LOADED', function( _e, _data ) {
 				self.loadCheck( _data );
@@ -70,12 +70,12 @@ var Bodin = function() {
 	 */
 	this.loadCheck = function( _data ) {
 		//------------------------------------------------------------
-		//  Mark instance as loaded.
+		//	Mark instance as loaded.
 		//------------------------------------------------------------
 		var id = jQuery( _data ).attr('id');
 		this.config[id]['loaded'] = true;
 		//------------------------------------------------------------
-		//  Check to see if all instances are loaded.
+		//	Check to see if all instances are loaded.
 		//------------------------------------------------------------
 		for ( var id in this.config ) {
 			if ( this.config[id]['loaded'] != true ) {
@@ -83,7 +83,7 @@ var Bodin = function() {
 			}
 		}
 		//------------------------------------------------------------
-		//  Loaded trigger
+		//	Loaded trigger
 		//------------------------------------------------------------
 		jQuery( window ).trigger( this.events['loaded'] );
 	}
@@ -94,7 +94,7 @@ var Bodin = function() {
  */
 var BodinAlign = function() {
 	//------------------------------------------------------------
-	//  This class is a singleton.  Ensure only one instance exists.
+	//	This class is a singleton.	Ensure only one instance exists.
 	//------------------------------------------------------------
 	if ( BodinAlign.prototype._singleton ) {
 		return BodinAlign.prototype._singleton;
@@ -108,10 +108,10 @@ var BodinAlign = function() {
 		loaded: 'BodinAlign-LOADED'
 	};
 	/**
-	 *  Start it up!
+	 *	Start it up!
 	 *
-	 *  @param { obj } _config Looks like this...
-	 * 		[
+	 *	@param { obj } _config Looks like this...
+	 *		[
 	 *			{ 
 	 *				src: 'tempXml/alignment.xml', 
 	 *				ids: [ 'latin', 'english' ] 
@@ -120,23 +120,23 @@ var BodinAlign = function() {
 	 */
 	this.start = function( _config ) {
 		//------------------------------------------------------------
-		//  Store the config for later use
+		//	Store the config for later use
 		//------------------------------------------------------------
 		this.config = _config;
 		//------------------------------------------------------------
-		//  Get the unique alignment source
+		//	Get the unique alignment source
 		//------------------------------------------------------------
 		for ( var i in _config ) {
 			this.src[ _config[i]['src'] ] = 1;
 		}
 		//------------------------------------------------------------
-		//  Create a container
+		//	Create a container
 		//------------------------------------------------------------
 		for ( var i in this.src ) {
 			this.xml[ i ] = { loaded: false };
 		}
 		//------------------------------------------------------------
-		//  Get each alignment xml
+		//	Get each alignment xml
 		//------------------------------------------------------------
 		for ( var i in this.src ) {
 			this._get( i );
@@ -144,9 +144,9 @@ var BodinAlign = function() {
 	}
 	
 	/**
-	 *  Retrieve an alignment xml document
+	 *	Retrieve an alignment xml document
 	 *
-	 *  @param { string } _src URL to an XML document
+	 *	@param { string } _src URL to an XML document
 	 */
 	this._get = function( _src ) {
 		var self = this;
@@ -162,7 +162,7 @@ var BodinAlign = function() {
 	}
 	
 	/**
-	 *  Check to see if each is loaded
+	 *	Check to see if each is loaded
 	 */
 	this.loadCheck = function() {
 		for ( var i in this.xml ) {
@@ -201,34 +201,134 @@ var BodinAlign = function() {
 	 */
 	BodinUI.prototype.init = function( _elem, _options ) {
 		var self = this;
-		
 		//------------------------------------------------------------
 		//	Mark your territory
 		//------------------------------------------------------------
 		jQuery( self.elem ).addClass('bodin');
-		
 		//------------------------------------------------------------
 		//	User options 
 		//------------------------------------------------------------
 		self.options = $.extend({}, _options );
-		
 		//------------------------------------------------------------
-		//  Events
+		//	Events
 		//------------------------------------------------------------
 		self.events = {
-			goTo: 'Bodin-GOTO',
-			align: 'Bodin-ALIGN'
+			goTo: 'BodinUI-GOTO'
 		};
-		
 		//------------------------------------------------------------
-		//  Build the thing
-		//------------------------------------------------------------
-		self.build();
-		
-		//------------------------------------------------------------
-		//  Start event listeners
+		//	Start event listeners
 		//------------------------------------------------------------
 		self.start();
+	}
+	
+	BodinUI.prototype.start = function() {
+		this.makeRoom();
+		this.sizeCheck();
+		this.buildNav();
+		this.tooltips();
+		//this.milestones();
+	}
+	
+	/**
+	 * Organize instances of bodin into columns
+	 *
+	 * @param {int } _count The number of instances.
+	 */
+	BodinUI.prototype.makeRoom = function() {
+		var count = jQuery( '.bodin' ).length
+		var styler = new Styler();
+		var percent = parseInt( 100/count );
+		styler.add({
+			'.bodin': 'width: '+percent+'%'
+		});
+	}
+
+	/**
+	* Check the size of the instance.
+	*/
+   BodinUI.prototype.sizeCheck = function() {
+		var self = this;
+		jQuery( window ).resize( function() {
+			if ( jQuery( self.elem ).width() < 450 ) {
+				jQuery( self.elem ).addClass('slim');
+			}
+			else {
+				jQuery( self.elem ).removeClass('slim');
+			}
+		});
+	}
+	
+	BodinUI.prototype.showMilestones = function() {
+		var self = this;
+		jQuery( 'p', self.elem ).each( function() {
+			console.log( this );
+		});
+		/*
+		this.tooltip.css({ 
+			left: pos_left, 
+			top: pos_top 
+		})*/
+	}
+	
+	/**
+	* Build the nav.
+	*/
+	BodinUI.prototype.buildNav = function() {
+		var self = this;
+		var id = jQuery( self.elem ).attr('id');
+		jQuery( self.elem ).prepend('\
+			<div id="sidecart_'+id+'"></div>\
+		');
+		var nav = '';
+		//------------------------------------------------------------
+		//  Get the nav by walking the dom
+		//------------------------------------------------------------
+		jQuery( '.edition', self.elem ).each( function() {
+			jQuery( '.book', this ).each( function() {
+				console.log( this );
+				jQuery( '.chapter', this ).each( function() {
+					console.log( this );
+				});
+			})
+		})
+		//------------------------------------------------------------
+		//  Start sidecart
+		//------------------------------------------------------------
+		jQuery( '#sidecart_'+id ).sidecart({
+			side: 'top',
+			inside: true,
+			views: [
+				{
+					id: id+'-view-1',
+					type: 'nav',
+					link: 'index',
+					src: '#content',
+					init: function() {},
+					refresh: function() {}
+				},
+				{
+					id: id+'-view-2',
+					type: 'extras',
+					link: 'extras',
+					src: '#content',
+					init: function() {},
+					refresh: function() {}
+				},
+				{
+					id: id+'-view-3',
+					type: 'config',
+					link: 'config',
+					src: '#content',
+					init: function() {},
+					refresh: function() {}
+				}
+			]
+		});
+	}
+	
+	BodinUI.prototype.tooltips = function() {
+		var id = jQuery( this.elem ).attr('id');
+		new Tooltipper( '#'+id+' .work' );
 	}
 	
 	//----------------
@@ -307,3 +407,112 @@ var BodinAlign = function() {
 		};
 	})
 })(jQuery);
+
+/**
+ * Tooltip
+ */
+function Tooltipper( _root ) {
+	this.root = _root;
+	this.tooltip = jQuery( '<div id="tooltipper"></div>' );
+	this.start();
+	this.target = null;
+}
+
+/**
+ * Start Tooltipper
+ */
+Tooltipper.prototype.start = function() {
+	var self = this;
+	//------------------------------------------------------------
+	//  Click listener
+	//------------------------------------------------------------
+	jQuery( self.root + ' [rel~=tooltip]' ).bind( 'touchstart click', function( _e ) {
+		_e.preventDefault();
+		//------------------------------------------------------------
+		//  Store reference to clicked element.
+		//------------------------------------------------------------
+		self.target = jQuery(this);
+		//------------------------------------------------------------
+		//  Remove existing tooltip
+		//------------------------------------------------------------
+		jQuery( self.root+' #tooltipper' ).remove();
+		//------------------------------------------------------------
+		//  Check for title
+		//------------------------------------------------------------
+		var tip = jQuery( this ).attr( 'title' );
+		if ( !tip || tip == '' ) {
+			return false;
+		}
+		//------------------------------------------------------------
+		//  Add the tooltip.
+		//------------------------------------------------------------
+		self.tooltip.css( 'opacity', 0 ).html( tip ).appendTo( 'body' );
+		//------------------------------------------------------------
+		//  Position it.
+		//------------------------------------------------------------
+		self.position();
+	});
+	//------------------------------------------------------------
+	//  Listen for events that require repositioning
+	//------------------------------------------------------------
+	jQuery( window ).resize( function() {
+		self.position() 
+	});
+	jQuery( self.root ).scroll( function() {
+		self.position() 
+	});
+}
+
+/**
+ * Position the tooltip
+ */
+Tooltipper.prototype.position = function() {
+	//------------------------------------------------------------
+	//  Only procceed if you have what you need.
+	//------------------------------------------------------------
+	if ( this.tooltip == undefined || this.target == undefined ) {
+		return;
+	}
+	//------------------------------------------------------------
+	//  Calculate the position of tooltip relative to the target.
+	//------------------------------------------------------------ 
+	if ( jQuery( window ).width() < this.tooltip.outerWidth() * 1.5 ) {
+		this.tooltip.css( 'max-width', jQuery( window ).width() / 2 );
+	}
+	else {
+		this.tooltip.css( 'max-width', 340 );
+	}
+	var pos_left = this.target.offset().left + ( this.target.outerWidth() / 2 ) - ( this.tooltip.outerWidth() / 2 );
+	var pos_top  = this.target.offset().top - this.tooltip.outerHeight() - 10;
+	if ( pos_left < 0 ) {
+		pos_left = this.target.offset().left + this.target.outerWidth() / 2 - 20;
+		this.tooltip.addClass( 'left' );
+	}
+	else {
+		this.tooltip.removeClass( 'left' );
+	}
+	if ( pos_left + this.tooltip.outerWidth() > jQuery( window ).width() ) {
+		pos_left = this.target.offset().left - this.tooltip.outerWidth() + this.target.outerWidth() / 2 + 20;
+		this.tooltip.addClass( 'right' );
+	}
+	else {
+		this.tooltip.removeClass( 'right' );
+	}
+	if ( pos_top < 0 ) {
+		var pos_top  = this.target.offset().top + this.target.outerHeight() + 10;
+		this.tooltip.addClass( 'top' );
+	}
+	else {
+		this.tooltip.removeClass( 'top' );
+	}
+	//------------------------------------------------------------
+	//  Position that tooltip!
+	//------------------------------------------------------------
+	this.tooltip.css({ 
+		left: pos_left, 
+		top: pos_top 
+	});
+	this.tooltip.animate({
+		opacity: 1 
+	}, 50 );
+}
